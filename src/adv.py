01fe6d87018c6bfdc4503e739d9fdd8141a3323e
Latosha Tims <ltims080913@hotmail.com>
2020-06-04 09:30:50 -0700

    adventure game setup

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -39,12 +40,19 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player("LTims", room['outside'])
+
+
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
-#
+
+while True:
+     print(f'You are {player.current_room.name}.  {player.current_room.description}')
+
+
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
